89a28172d77cf12e66d5afb401986fc359dca183
Nat Welch <nat@natwelch.com>
2011-08-29 18:21:22 -0700

    Basics of Dropbox auth.

--- app/controllers/account.rb
+++ app/controllers/account.rb
@@ -1,16 +1,18 @@
 Mymemoryreallysucks.controllers :account do
    get :index do
-      # Box.net
-      account = Box::Account.new(BOX_API_KEY)
-
-      if !account.authorized?
-         account.authorize(nil) do |auth_url|
-            redirect auth_url
-            p auth_url
-            render "/account/index", :locals => { "account" => account, "box_auth_url" => auth_url}
+      if params[:oauth_token] and !session[:dropbox_session].nil? then
+         dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
+         if !dropbox_session.authorized?
+            dropbox_session.authorize(params)
          end
+         session[:dropbox_session] = dropbox_session.serialize # re-serialize the authenticated session
+
+         p session
+         render "/account/index", :locals => { "account" => session, "box_auth_url" => "#" }
       else
-         render "/account/index", :locals => { "account" => account, "box_auth_url" => "#" }
+         dropbox_session = Dropbox::Session.new('xndjpji4o7ynl6l', 't3f0hrbh0a23i27')
+         session[:dropbox_session] = dropbox_session.serialize
+         redirect dropbox_session.authorize_url( :oauth_callback => request.url )
       end
    end
 end
